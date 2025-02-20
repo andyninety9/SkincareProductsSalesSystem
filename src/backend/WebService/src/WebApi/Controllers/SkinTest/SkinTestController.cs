1430f764f17cddc93fc64ab63d82b69fc96fdf59
andy.ninety9 <114195582+andyninety9@users.noreply.github.com>
2025-02-20 19:13:31 +0700

    UPDATE: add validator for get result request

--- src/backend/WebService/src/WebApi/Controllers/SkinTest/SkinTestController.cs
+++ src/backend/WebService/src/WebApi/Controllers/SkinTest/SkinTestController.cs
@@ -1,6 +1,7 @@
 using System.Security.Claims;
 using Application.Constant;
 using Application.Features.SkinTest.Queries;
+using Application.Features.SkinTest.Queries.Validators;
 using Application.SkinTest.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
@@ -95,21 +96,40 @@ namespace WebApi.Controllers.SkinTest
         {
             if (User == null)
             {
-                return Unauthorized(new { statusCode = 401, message = IConstantMessage.USER_INFORMATION_NOT_FOUND });
+            return Unauthorized(new { statusCode = 401, message = IConstantMessage.USER_INFORMATION_NOT_FOUND });
             }
 
             var usrID = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
             if (string.IsNullOrEmpty(usrID))
             {
-                return Unauthorized(new { statusCode = 401, message = IConstantMessage.MISSING_USER_ID });
+            return Unauthorized(new { statusCode = 401, message = IConstantMessage.MISSING_USER_ID });
             }
 
             if (!long.TryParse(usrID, out var userId))
             {
-                return Unauthorized(new { statusCode = 401, message = IConstantMessage.INTERNAL_SERVER_ERROR });
+            return Unauthorized(new { statusCode = 401, message = IConstantMessage.INTERNAL_SERVER_ERROR });
             }
-            var query = new GetQuizResultQuery(userId, long.Parse(quizId));
+
+            if (string.IsNullOrEmpty(quizId))
+            {
+            return BadRequest(new { statusCode = 400, message = "Quiz ID is required" });
+            }
+
+            if (!long.TryParse(quizId, out var parsedQuizId))
+            {
+            return BadRequest(new { statusCode = 400, message = "Invalid Quiz ID format" });
+            }
+
+            var query = new GetQuizResultQuery(userId, parsedQuizId);
+            var validator = new GetQuizResultQueryValidator();
+            var validationResult = await validator.ValidateAsync(query, cancellationToken);
+
+            if (!validationResult.IsValid)
+            {
+            return BadRequest(new { statusCode = 400, message = validationResult.Errors.First().ErrorMessage });
+            }
+
             var result = await _mediator.Send(query, cancellationToken);
             return Ok(new { statusCode = 200, message = "Get result successfully", data = result.Value });
         }
