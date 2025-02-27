a236ea489eb75d2355e9359525ccbb6236f692df
Thảo Vi <146309437+ThDziii@users.noreply.github.com>
2025-02-27 15:15:32 +0700

    add redux

---
+++ src/frontend/src/config/api.jsx
@@ -0,0 +1,53 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+import { jwtDecode } from "jwt-decode";
+
+const baseUrl = "https://www.mavid.store/api/";
+
+const config = {
+  baseUrl,
+  timeout: 3000000,
+};
+const api = axios.create(config);
+api.defaults.baseURL = baseUrl;
+
+const handleBefore = async (config) => {
+  let accessToken = Cookies.get("accessToken")?.replaceAll('"', "");
+
+  if (accessToken) {
+    const tokenExpiry = jwtDecode(accessToken).exp * 1000;
+    if (Date.now() >= tokenExpiry) {
+      try {
+        const refreshToken = Cookies.get("refreshToken");
+        const encodedRefreshToken = encodeURIComponent(refreshToken);
+        const response = await axios.post(
+          `https://localhost:5001/api/Authentication/refresh-token?refreshToken=${encodedRefreshToken}`
+        );
+        console.log(response);
+        accessToken = response.data.token;
+        Cookies.set("accessToken", response.data?.token, {
+          expires: 7,
+          secure: true,
+        }); // Expires in 7 days
+        Cookies.set("refreshToken", response.data?.refreshToken, {
+          expires: 7,
+          secure: true,
+        });
+      } catch (error) {
+        console.error("Failed to refresh token:", error);
+        return Promise.reject(error);
+      }
+    }
+    config.headers.Authorization = `Bearer ${accessToken}`;
+  }
+  return config;
+};
+
+const handleError = (error) => {
+  console.error("API Error:", error);
+  return Promise.reject(error);
+};
+
+api.interceptors.request.use(handleBefore, handleError);
+
+export default api;
