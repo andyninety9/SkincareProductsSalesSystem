c482c39f98822117fb75fb099ecd8c3a989ae982
Mai Anh Duy <114195582+andyninety9@users.noreply.github.com>
2025-03-03 18:53:37 +0700

    FEAD: handle logout and handle errors login

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { routes } from "./routes";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { routes } from './routes';
 import AboutPage from './page/aboutPage/aboutPage';
 import Login from './page/login/Login';
 import Register from './page/register/Register';
@@ -19,62 +19,68 @@ import CheckOutPage from './page/checkoutPage/CheckoutPage';
 import EventPage from './page/eventPage/EventPage';
 import AboutUs from './page/aboutUsPage/AboutUs';
 import ProfilePage from './page/profilePage/ProfilePage';
-import QuizPage from "./page/quizPage/QuizPage";
-import StartQuizPage from "./page/quizPage/StartQuizPage";
-import ManageOrderPage from "./page/manageOrder/ManageOrder";
-import ManageCommentPage from "./page/manageComment/ManageComment";
-import ResultPage from "./page/quizPage/ResultPage";
-import MainLayout from "./layout/mainLayout/mainLayout";
+import QuizPage from './page/quizPage/QuizPage';
+import StartQuizPage from './page/quizPage/StartQuizPage';
+import ManageOrderPage from './page/manageOrder/ManageOrder';
+import ManageCommentPage from './page/manageComment/ManageComment';
+import ResultPage from './page/quizPage/ResultPage';
+import MainLayout from './layout/mainLayout/mainLayout';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: routes.login,
-      element: <Login />,
-    },
+    const router = createBrowserRouter([
+        {
+            path: routes.login,
+            element: <Login />,
+        },
 
-    {
-      path: routes.register,
-      element: <Register />,
-    },
-    {
-      path: routes.resetPassword,
-      element: <ResetPassword />,
-    },
-    {
-      path: routes.manageOrder,
-      element: <ManageOrderPage />,
-    },
-    {
-      path: routes.manageComment,
-      element: <ManageCommentPage />,
-    },
+        {
+            path: routes.register,
+            element: <Register />,
+        },
+        {
+            path: routes.resetPassword,
+            element: <ResetPassword />,
+        },
+        {
+            path: routes.manageOrder,
+            element: <ManageOrderPage />,
+        },
+        {
+            path: routes.manageComment,
+            element: <ManageCommentPage />,
+        },
 
-    {
-      path: routes.home,
-      element: <UserLayout />,
-      children: [
-        { path: routes.home, element: <HomePage /> },
-        { path: routes.about, element: <AboutPage /> },
-        { path: routes.product, element: <MainLayout /> },
-        { path: "/product/:id", element: <ProductDetailPage /> },
-        { path: routes.review, element: <ReviewPage /> },
-        { path: routes.contact, element: <Contact /> },
-        { path: routes.service, element: <Service /> },
-        { path: routes.faq, element: <Faq /> },
-        { path: routes.cart, element: <CartPage /> },
-        { path: routes.checkout, element: <CheckOutPage /> },
-        { path: routes.event, element: <EventPage /> },
-        { path: routes.aboutUs, element: <AboutUs /> },
-        { path: routes.profile, element: <ProfilePage /> },
-        { path: routes.quiz, element: <QuizPage /> },
-        { path: routes.startQuiz, element: <StartQuizPage /> },
-        { path: routes.resultQuiz, element: <ResultPage /> },
-      ],
-    },
-  ]);
+        {
+            path: routes.home,
+            element: <UserLayout />,
+            children: [
+                { path: routes.home, element: <HomePage /> },
+                { path: routes.about, element: <AboutPage /> },
+                { path: routes.product, element: <MainLayout /> },
+                { path: '/product/:id', element: <ProductDetailPage /> },
+                { path: routes.review, element: <ReviewPage /> },
+                { path: routes.contact, element: <Contact /> },
+                { path: routes.service, element: <Service /> },
+                { path: routes.faq, element: <Faq /> },
+                { path: routes.cart, element: <CartPage /> },
+                { path: routes.checkout, element: <CheckOutPage /> },
+                { path: routes.event, element: <EventPage /> },
+                { path: routes.aboutUs, element: <AboutUs /> },
+                { path: routes.profile, element: <ProfilePage /> },
+                { path: routes.quiz, element: <QuizPage /> },
+                { path: routes.startQuiz, element: <StartQuizPage /> },
+                { path: routes.resultQuiz, element: <ResultPage /> },
+            ],
+        },
+    ]);
 
-  return <RouterProvider router={router} />;
+    return (
+        <>
+            <Toaster position="top-right" reverseOrder={false} />
+            <RouterProvider router={router} />
+        </>
+    );
 }
 
 export default App;
