468aa809831362874b4df6d2aa8c36791287cfbe
Mai Anh Duy <114195582+andyninety9@users.noreply.github.com>
2025-03-11 20:05:38 +0700

    FEAD: add api create new brand

---
+++ src/backend/WebService/src/Application/Features/Brands/Commands/Validators/CreateProductBrandCommandValidator.cs
@@ -0,0 +1,28 @@
+using FluentValidation;
+
+namespace Application.Features.Brands.Commands.Validators
+{
+    public class CreateProductBrandCommandValidator : AbstractValidator<CreateProductBrandCommand>
+    {
+        public CreateProductBrandCommandValidator()
+        {
+            RuleFor(x => x.BrandName)
+                .NotEmpty().WithMessage("Brand Name is required")
+                .NotNull().WithMessage("Brand Name is required");
+
+            RuleFor(x => x.BrandDesc)
+                .NotEmpty().WithMessage("Brand Description is required")
+                .NotNull().WithMessage("Brand Description is required");
+
+            RuleFor(x => x.BrandOrigin)
+                .NotEmpty().WithMessage("Brand Origin is required")
+                .NotNull().WithMessage("Brand Origin is required");
+
+            RuleFor(x => x.BrandStatus)
+                .NotEmpty().WithMessage("Brand Status is required")
+                .NotNull().WithMessage("Brand Status is required")
+                .Must(x => x == 1 || x == 0).WithMessage("Brand Status must be 1 or 0");
+        }
+        
+    }
+}
