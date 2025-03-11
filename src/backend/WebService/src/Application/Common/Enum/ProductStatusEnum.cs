1db37115d9311c54d01ec34f3bb948ffa0460f94
Mai Anh Duy <114195582+andyninety9@users.noreply.github.com>
2025-03-11 18:05:51 +0700

    FEAD: delete product api

---
+++ src/backend/WebService/src/Application/Common/Enum/ProductStatusEnum.cs
@@ -0,0 +1,11 @@
+namespace Application.Common.Enum
+{
+    public enum ProductStatusEnum
+    {
+        Available = 1,
+        OutOfStock = 2,
+        Discontinued = 3,
+        AwaitingRestock = 4,
+        OnSale = 5
+    }
+}
