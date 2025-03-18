16aafb0bca12e2683b31338ac54f108cd155ff23
Mai Anh Duy <114195582+andyninety9@users.noreply.github.com>
2025-03-19 00:10:18 +0700

    FEAD: add verify email page

---
+++ src/frontend/src/page/verifyEmailPage/VerifyEmailPage.jsx
@@ -0,0 +1,8 @@
+export default function VerifyEmailPage() {
+    return (
+        <div className="container">
+            <h1 className="text-center">Verify Email</h1>
+            <p className="text-center">Please wait...</p>
+        </div>
+    );
+}
