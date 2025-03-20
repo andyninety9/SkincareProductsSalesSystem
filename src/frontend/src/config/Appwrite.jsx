de56c17a764a2833711c45b7d5777ae54a621f42
Mai Anh Duy <114195582+andyninety9@users.noreply.github.com>
2025-03-20 13:34:37 +0700

    UPDATE: APPWRITE

--- src/frontend/src/config/Appwrite.jsx
+++ src/frontend/src/config/Appwrite.jsx
@@ -1,9 +1,12 @@
-import { Client, Storage } from "appwrite";
+import { Client, Storage } from 'appwrite';
 
 const client = new Client();
 
 client
-  .setEndpoint("https://cloud.appwrite.io/v1") // Your Appwrite Endpoint
-  .setProject("67cf89ff00003c9bbf4e"); // Your project ID
+    .setEndpoint('https://cloud.appwrite.io/v1') // Your Appwrite Endpoint
+    .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID); // Your project ID
+// client
+// .setEndpoint("https://cloud.appwrite.io/v1") // Your Appwrite Endpoint
+// .setProject("67cf89ff00003c9bbf4e"); // Your project ID
 
 export const storage = new Storage(client);
